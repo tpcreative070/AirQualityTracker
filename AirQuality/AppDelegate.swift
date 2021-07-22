7c1336f197e1cedb22322f71b6f013870b3ff563
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2021-07-22 21:27:47 +0700

    [mater] - Initialized project

--- AirQuality/AppDelegate.swift
+++ AirQuality/AppDelegate.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyCJAof6Ht3LIqoJHAR5DFW32idcV8MvjZ4")
         return true
     }
 
--- AirQuality/ViewController.swift
+++ AirQuality/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let camera = GMSCameraPosition.camera(withLatitude: 10.823370, longitude:106.638580, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        self.view.addSubview(mapView)
+               // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 10.823370, longitude: 106.638580)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
-
-
 }
 
